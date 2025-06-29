ff951cf0e546ad8d332712fc815239ee8ca493f4
amanrajsaxena8394 <amanrajsaxena502@gmail.com>
2025-06-29 17:08:33 +0530

    Added Dotenv

--- src/main/java/com/example/demo/EcommerceSpringApplication.java
+++ src/main/java/com/example/demo/EcommerceSpringApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.DotenvEntry;
+ 
+
 
 @SpringBootApplication //enabling few spring boot features starting point 
 public class EcommerceSpringApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv=Dotenv.configure().load();
+	//	dotenv.entries().forEach(DotenvEntry entry -> System.setProperty(entry.getKey(),entry.getValue()));
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(EcommerceSpringApplication.class, args);
-	}
+
+		//kkkk
+	} 
 
 }
+
+//./gradlew clean build --refresh-dependencies
